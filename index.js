1166467a1383a4b08addb9f0b4cdb229c6e7ed5a
Sergey Jar <sereja@d42.tech>
2021-08-30 12:37:33 +0300

    added test script

---
+++ index.js
@@ -0,0 +1,15 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setViewport({
+    width: 1920,
+    height: 1080,
+  });
+  await page.goto('https://dev.tms42.com/');
+  await page.screenshot({ path: 'example.png' });
+  await page.pdf({ path: 'example.pdf' });
+
+  await browser.close();
+})();
